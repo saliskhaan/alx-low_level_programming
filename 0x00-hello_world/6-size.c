abe11a58800e9202ad13deda6cf40ea073952416
Saliskhaan <Saliskhaan@gmail.com>
2022-12-08 12:05:18 -0800

    C Programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,11 @@ int A;
 long int B;
 long long int C;
 char D;
-float E; 
+float E;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(D));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(A));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(B));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(C));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(E));
- return (0);}
+return (0);
+}
