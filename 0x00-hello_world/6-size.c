03c592a54ec536ee34329f75e0433c4a1c4f2049
Saliskhaan <Saliskhaan@gmail.com>
2022-12-08 11:15:14 -0800

    C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*  main - Entry point
+*
+*  Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+int A;
+long int B;
+long long int C;
+char D;
+float E;
+
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(A));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(B));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(C));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(D));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(E));
+return (0);
+}
