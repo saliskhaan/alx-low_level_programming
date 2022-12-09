c5704bc3c41d0497534df256b4674398460779bd
Saliskhaan <Saliskhaan@gmail.com>
2022-12-09 12:31:47 -0800

    Lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* main - Printing the lowercase alphabet in reverse order.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'z'; alphabet >= 'z'; alphabet--)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
